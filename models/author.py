a4f46a143f6084e91e29ad0b44700b0822d27c8b
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-12-16 14:39:13 +0300

    last commit

--- models/author.py
+++ models/author.py
@@ -1,7 +1,20 @@
 class Author:
-    def __init__(self, id, name):
+    def __init__(self, id, name):  
         self.id = id
         self.name = name
 
-    def __repr__(self):
+    def __repr__(self): 
         return f'<Author {self.name}>'
+
+    # Getter for name
+    @property
+    def name(self):
+        return self._name
+
+
+    @name.setter #setter for name to enforce the parameters passed ...
+    def name(self, value):
+        if not hasattr(self, '_name'):
+            self._name = value
+        else:
+            raise ValueError("Name cannot be changed after instantiation.")
